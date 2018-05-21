8132c937989b23f10cd67fac0625930c504b7d2a
Adria Castany <adria.castany@haufe.com>
2018-05-21 19:35:30 +0200

    Add Map and applied a style

--- App.js
+++ App.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { MapView, Location, Permissions } from "expo";
+
+import { mapStyle } from "./assets/mapStyles/strangerNightVision";
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <MapView
+          provider={'google'}
+          style={styles.map}
+          customMapStyle={mapStyle}
+          showsCompass={false}
+          initialRegion={{ latitude: 41.3916097, longitude: 2.1769906, latitudeDelta: 0.0043, longitudeDelta: 0.0034 }}
+          showsUserLocation={true}
+          followsUserLocation={true}
+        >
+        </MapView>
       </View>
     );
   }
@@ -17,5 +29,8 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  map: {
+    ...StyleSheet.absoluteFillObject,
   },
 });
